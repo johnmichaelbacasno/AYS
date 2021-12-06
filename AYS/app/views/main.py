a1d1087da7f5891e84f01eb8289c86b06119369f
John Michael Bacasno <johnmichael.bacasno@g.msuiit.edu.ph>
2021-12-06 19:55:21 +0800

    Add terms and conditions

--- AYS/app/views/main.py
+++ AYS/app/views/main.py
@@ -4,4 +4,4 @@ main = Blueprint('main', __name__)
 
 @main.route('/')
 def main_content():
-    return render_template('privacy_policy.html')
+    return render_template('terms_and_conditions.html')
