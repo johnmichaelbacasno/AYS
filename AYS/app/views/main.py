49ce3b77546faee8bd4a98c55ea8dde19ab53286
John Michael Bacasno <johnmichael.bacasno@g.msuiit.edu.ph>
2021-12-07 15:03:16 +0800

    Add backend

--- AYS/app/views/main.py
+++ AYS/app/views/main.py
@@ -8,7 +8,12 @@ main = Blueprint('main', __name__)
 def all_services():
     conn = db.connect()
     cursor = conn.cursor(pymysql.cursors.DictCursor)
-    cursor.execute("SELECT * FROM `Service`")
+    cursor.execute("""
+        SELECT Service.service_title, Service.service_description, Service.service_user_id, Service.service_user_rating, Service.service_user_level, Service.service_user_is_trusted, Service.service_date_posted, Service.service_schedule, Service.service_location, ServiceType.service_type_name AS service_type, ServiceCategory.service_category_name AS service_category, Service.service_amount
+        FROM `Service`
+        JOIN `ServiceType` ON Service.service_type = ServiceType.service_type_id
+        JOIN `ServiceCategory` ON ServiceType.service_category = ServiceCategory.service_category_id
+    """)
     services = cursor.fetchall()
     cursor.close()
     conn.close()
