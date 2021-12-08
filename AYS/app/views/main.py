2c0c3e312447117112871a43996cc7415274ea56
John Michael Bacasno <johnmichael.bacasno@g.msuiit.edu.ph>
2021-12-08 22:17:56 +0800

    Improve UI

--- AYS/app/views/main.py
+++ AYS/app/views/main.py
@@ -4,11 +4,18 @@ from .. manage import *
 
 main = Blueprint('main', __name__)
 
-@main.route('/')
-def main_content():
+@main.route('/explore_services')
+def explore_services():
     return render_template(
         'explore_services.html',
         services=all_services(),
         featured_service_categories=featured_service_categories(),
         featured_service_providers=featured_service_providers()
+    )
+
+@main.route('/')
+@main.route('/landing_page_service_provider')
+def landing_page_service_provider():
+    return render_template(
+        'landing_page_service_provider.html'
     )
